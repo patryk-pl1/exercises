7abdabe3685dc28cf9b38b1dc4dd2528a6378c9c
patryk-pl1 <81013592+patryk-pl1@users.noreply.github.com>
2021-08-16 09:57:06 +0200

    nastpny

--- javas.js
+++ javas.js
@@ -1,13 +1,55 @@
 
-var przycisk = document.getElementById('klik');
-przycisk.onclick = (e) => 
-    console.log(e);
 
-class Car {
-    constructor(model, year) {
-        this.model = model;
-        this.year = year;
-    }
+// const f1 = (callback) => {
+//     setTimeout(() => {
+//         console.log('f1');
+//         callback();
+//     }, 500)
+// }
+// const f2 = (callback) => {
+//     setTimeout(() => {
+//         console.log('f2');
+//         callback();
+//     }, 300)
+// }
+// const f3 = () => {
+//     setTimeout(() => {
+//          console.log('f3')
+//      }, 100)
+//  }
+
+// new Promise ((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log('1');
+//         resolve("to jest z obietnicy");
+//     }, 400)
+// })
+// .then((response) => {
+//     console.log("end!");
+//     console.log(response);
+//     return response;
+// })
+// .then((res) => {
+//     console.log('Res ' + res);
+// })
+
+const getUserData = () => {
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            console.log('1');
+           // resolve("to jest odpowiedź z obietnicy");
+           reject('błąd');
+        }, 600)
+    })
 }
 
-console.log(typeof Car);
+getUserData().then((res) => {console.log(res)}).catch(err => console.error(err));
+
+var obietnica = new Promise((resolve, reject) => {
+    setTimeout(
+        () => {
+            console.log('timeout');
+            resolve();    
+        }, 1000);
+})
