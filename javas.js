1f8ddb64098c6c6c995430bad70c8a2342bf8fa0
patryk-pl1 <81013592+patryk-pl1@users.noreply.github.com>
2021-08-18 19:16:41 +0200

    commit z opisem
    szczegóowy opis

--- javas.js
+++ javas.js
@@ -2,3 +2,10 @@
 fetch('https://reqres.in/api/users')
     .then(response => response.json())
     .then(data => console.log(data));
+
+const dodanyObiekt = {
+    a: 3,
+    b: 4
+}
+
+const zmienna = 5;
