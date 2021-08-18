7719f863847a46ce43820235b92ca12d7cc95891
patryk-pl1 <81013592+patryk-pl1@users.noreply.github.com>
2021-08-18 19:08:33 +0200

    commit

--- javas.js
+++ javas.js
@@ -1,10 +1,4 @@
-const obiekt = {
-    a: 1,
-    b: 2,
-    c: 3,
-}
 
-for (let param in obiekt) {
-    console.log(`${param} oraz ${obiekt[param]}`);
-    console.log(param)
-}
+fetch('https://reqres.in/api/users')
+    .then(response => response.json())
+    .then(data => console.log(data));
