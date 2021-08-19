7064f85b57df01407bba65d4c87a8e7b1af20927
patryk-pl1 <81013592+patryk-pl1@users.noreply.github.com>
2021-08-19 17:52:03 +0200

    moje
    opis

--- javas.js
+++ javas.js
@@ -1,2 +1,10 @@
 
 
+
+
+function handleToast() {
+    const toast = document.getElementById('toastid');
+    console.log("kliknięto")
+    toast.style = "display: flex;" +
+        "position: fixed; bottom: 25px; right: 15px;";
+}
