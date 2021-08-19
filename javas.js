1540eba372c01925135a03aebd55b679e3e6612e
patryk-pl1 <81013592+patryk-pl1@users.noreply.github.com>
2021-08-19 14:51:45 +0200

    pusty
    opis

--- javas.js
+++ javas.js
@@ -1,13 +1,2 @@
 
-fetch('https://reqres.in/api/users')
-    .then(response => response.json())
-    .then(data => console.log(data));
 
-const dodanyObiekt = {
-    a: 3,
-    b: 4
-}
-
-const zmienna = 5;
-
-var nowazmienna;
